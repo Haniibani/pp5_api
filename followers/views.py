6294ccd32ee9e1f93d8e714cba775bcc866cc63a
Haniibani <hanna.behmer@gmail.com>
2024-01-13 15:52:44 +0000

    add followers feature

---
+++ followers/views.py
@@ -0,0 +1,30 @@
+from rest_framework import generics, permissions
+from pp5_api.permissions import IsOwnerOrReadOnly
+from .models import Follower
+from .serializers import FollowerSerializer
+
+
+class FollowerList(generics.ListCreateAPIView):
+    """
+    List all followers - all instances of a user
+    following another user.
+    Create a follower - follow a user if logged in.
+    Perform_create - associate the current logged in user with a follower.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+    """
+    Retrieves a follower
+    No Update view, as we either follow or unfollow users
+    Destroy or delete a follower, i.e. unfollow someone if owner
+    """
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Follower.objects.all()
+    serializer_class = FollowerSerializer
