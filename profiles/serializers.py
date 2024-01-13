9975107d26cc3935bda3c27362ed98ad5040cac2
Haniibani <hanna.behmer@gmail.com>
2024-01-13 15:59:43 +0000

    add followers to profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -5,15 +5,26 @@ from .models import Profile
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner
+            ).first()
+            # print(following)
+            return following.id if following else None
+        return None
 
     class Meta:
         model = Profile
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'name',
             'content', 'image', 'facebook_link', 'twitter_link',
-            'linkedin_link', 'is_owner'
+            'linkedin_link', 'is_owner', 'following_id',
         ]
