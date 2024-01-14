a2d35657a93e1a21a1816305431e99a1104e3060
Haniibani <hanna.behmer@gmail.com>
2024-01-14 14:09:56 +0000

    add search functionality through django-filter

--- comments/views.py
+++ comments/views.py
@@ -1,19 +1,19 @@
 
 from django.http import Http404
 from rest_framework import status, generics, permissions
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Comment
 from .serializers import CommentSerializer, CommentDetailSerializer
 from pp5_api.permissions import IsOwnerOrReadOnly  
 
-# Create CommentList APIView
 class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly
     ]
     Queryset = Comment.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['post']
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
