23e54128babf4a6c0d9f7a3ba0fc1cb0edf75d47
Haniibani <hanna.behmer@gmail.com>
2024-01-20 17:38:54 +0000

    Fix the signals for notifications due to mismatch

--- notifications/signals.py
+++ notifications/signals.py
@@ -7,32 +7,36 @@ from comments.models import Comment
 from likes.models import Like
 from followers.models import Follower 
 
+# For creating comment notifications
 @receiver(post_save, sender=Comment)
 def create_comment_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(
-            recipient=instance.post.author,
-            message=f"New comment on your post by {instance.author.username}",
+            recipient=instance.post.owner,  # Use 'owner' instead of 'author'
+            message=f"New comment on your post by {instance.owner.username}",
             notification_type='comment',
             related_object_id=instance.post.id
         )
 
+# For creating like notifications
 @receiver(post_save, sender=Like)
 def create_like_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(
-            recipient=instance.post.author,
-            message=f"Your post was liked by {instance.user.username}",
+            recipient=instance.post.owner,  # Use 'owner' instead of 'author'
+            message=f"Your post was liked by {instance.owner.username}",
             notification_type='like',
             related_object_id=instance.post.id
         )
 
+# For creating follower notifications
 @receiver(post_save, sender=Follower)
 def create_follower_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(
             recipient=instance.followed,
-            message=f"You have a new follower: {instance.follower.username}",
+            message=f"You have a new follower: {instance.owner.username}",  # Use 'owner' instead of 'follower'
             notification_type='follower',
-            related_object_id=instance.follower.id
+            related_object_id=instance.owner.id
         )
+
