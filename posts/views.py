60873e2573061591e88c9631b1257a8840dfe08d
Haniibani <hanna.behmer@gmail.com>
2024-01-21 08:00:28 +0000

    Fix the posts assosiation with tags

--- posts/views.py
+++ posts/views.py
@@ -5,48 +5,43 @@ from .models import Post, Tag
 from .serializers import PostSerializer
 from pp5_api.permissions import IsOwnerOrReadOnly
 
-class PostList(generics.ListCreateAPIView):
 
+class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.annotate(
-        likes_count=Count('likes', distinct=True),
-        comments_count=Count('comment', distinct=True)
-    ).order_by('-created_at')
+        likes_count=Count("likes", distinct=True),
+        comments_count=Count("comment", distinct=True),
+    ).order_by("-created_at")
     filter_backends = [
         filters.OrderingFilter,
         filters.SearchFilter,
         DjangoFilterBackend,
     ]
     filterset_fields = [
-        'owner__followed__owner__profile',
-        'likes__owner__profile',
-        'owner__profile',
-        'tags'
+        "owner__followed__owner__profile",
+        "likes__owner__profile",
+        "owner__profile",
+        "tags",
     ]
     search_fields = [
-        'owner__username',
-        'title',
+        "owner__username",
+        "title",
     ]
     ordering_fields = [
-        'likes_count',
-        'comments_count',
-        'likes__created_at',
+        "likes_count",
+        "comments_count",
+        "likes__created_at",
     ]
 
     def perform_create(self, serializer):
-
-        tags_data = self.request.data.get('tags', [])
-        tags = Tag.objects.filter(name__in=tags_data)
-        post = serializer.save(owner=self.request.user)
-        post.tags.set(tags)
+        serializer.save(owner=self.request.user)
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.annotate(
-        likes_count=Count('likes', distinct=True),
-        comments_count=Count('comment', distinct=True)
-    ).order_by('-created_at')
+        likes_count=Count("likes", distinct=True),
+        comments_count=Count("comment", distinct=True),
+    ).order_by("-created_at")
