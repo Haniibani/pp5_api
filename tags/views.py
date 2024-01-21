63577234cf20e6325783068a547366b9e627eed0
Haniibani <hanna.behmer@gmail.com>
2024-01-21 08:14:41 +0000

    Fix Tags to only use the predefined ones in the database

--- tags/views.py
+++ tags/views.py
@@ -6,12 +6,3 @@ class TagList(generics.ListAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def get_serializer(self, *args, **kwargs):
-        """
-        Override the default get_serializer method to use TagListForm
-        for creating a new tag.
-        """
-        if self.request.method == 'POST':
-            return TagListForm(*args, **kwargs)
-        return super().get_serializer(*args, **kwargs)
